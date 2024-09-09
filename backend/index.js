9bc40040bfb08c137ae7c063919346bba31212b8
vickyiitbhu <vicky.bijarniya.civ20@itbhu.ac.in>
2024-09-10 00:31:07 +0530

    change index.js

--- backend/index.js
+++ backend/index.js
@@ -4,20 +4,19 @@ const port = 5000
 const mongoDB=require('./db')
 
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin","*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type,Accept"
   );
   next();
 })
-
+app.use(express.json());
 mongoDB();
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use(express.json());
 app.use('/api',require("./Routes/CreateUser"));
 app.use('/api',require("./Routes/DisplayData"));
 app.use('/api',require("./Routes/Orderdata"));  
